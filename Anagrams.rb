17ccca447b4b72e4f8f5f568c07f2c9d2f943f16
Syldys <syldysnya@gmail.com>
2021-03-19 16:52:16 -0400

    Add #second_anagram

--- Anagrams.rb
+++ Anagrams.rb
@@ -1,30 +1,35 @@
 def first_anagram?(str1, str2)
     poss_str = []
     
-    (0...str1.length).each do |i|
-        (0...str1.length).each do |i2|
-            poss_str << str1[i..i2] unless str1[i..i2].empty?
-            # poss_str << str1[0..i2].reverse
-            i = 1
-            str3 = str1.split.rotate(i)
-            (str1.length - 1).times do 
-                poss_str << str3[i..i2].join(" ") unless str3[i..i2].empty?
-                i += 1
-            end 
-        end 
-    end 
+    # (0...str1.length).each do |i|
+    #     (0...str1.length).each do |i2|
+    #         poss_str << str1[i..i2] unless str1[i..i2].empty?
+    #         # poss_str << str1[0..i2].reverse
+    #         i = 1
+    #         str3 = str1.split.rotate(i)
+    #         (str1.length - 1).times do 
+    #             poss_str << str3[i..i2].join(" ") unless str3[i..i2].empty?
+    #             i += 1
+    #         end 
+    #     end 
+    # end 
     poss_str
 end 
 
-p first_anagram?("gizmo", "sally")    #=> false
-p first_anagram?("elvis", "lives")
+# first_anagram?("gizmo", "sally")    #=> false
+# first_anagram?("elvis", "lives")
+
+def second_anagram?(str1, str2)
+    str1.each_char.with_index do |char, i|
+        if str2.index(str1[i])
+            str2[str2.index(str1[i])] = ""
+        end
+    end
+   str2.empty?
+end
+
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")
 
 
-[
-"gizmo"
-"gizom"
-"gimzo"
-"gimoz"
-"g"
-]
 
