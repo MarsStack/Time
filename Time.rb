213c75064b3362e91a52e907b39842d67505a0af
Syldys <syldysnya@gmail.com>
2021-03-19 15:01:27 -0400

    Add #largest_contiguous_subsum

--- Time.rb
+++ Time.rb
@@ -28,6 +28,38 @@ end
 
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min_phase2(list)  # =>  O(n)
-p my_min_phase1(list)  # =>  O(n^2)
+my_min_phase2(list)  # =>  O(n)
+my_min_phase1(list)  # =>  O(n^2)
 
+def largest_contiguous_subsum(arr)
+    subarrs = []
+
+    (0...arr.length).each do |i1|
+        (i1...arr.length).each do |i2|
+            subarrs << arr[i1..i2]
+        end
+    end
+    
+    sums = []
+    subarrs.each do |sub|
+        sums << sub.sum
+    end
+    sums.max
+end
+
+list2 = [5, 3, -7]
+p largest_contiguous_subsum(list2) # => 8
+
+## possible sub-sums
+#[5]           # => 5
+#[5, 3]        # => 8 --> we want this one
+#[5, 3, -7]    # => 1
+#[3]           # => 3
+#[3, -7]       # => -4
+#[-7]          # => -7
+
+list3 = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list3) # => 8 (from [7, -6, 7])
+
+list4 = [-5, -1, -3]
+p largest_contiguous_subsum(list4) # => -1 (from [-1])
